8b3fc28da51a706361d5a31696e64e67d11bfe29
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-06-27 18:39:16 -0300

    first commit

---
+++ src/app/styles/themes/default.ts
@@ -0,0 +1,28 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+
+    'modal-overlay': '#7c8c98',
+
+    'blue-300': '#f0f8ff',
+    'blue-400': '#067eff',
+    'blue-500': '#0a83ff',
+    'blue-600': '#578797',
+    'blue-700': '#1c3d51',
+
+    'gray-300': '#f2f4f8',
+    'gray-400': '#d5dbde',
+    'gray-500': '#c9d4dd',
+    'gray-600': '#8a93ae',
+    'gray-700': '#212121',
+
+    'green-400': '#c2f4eb',
+    'green-500': '#9fefdf',
+
+    'light-blue-400': '#c2e7ff',
+    'light-blue-500': '#a1d9ff',
+
+    'yellow-400': '#fff8d0',
+    'yellow-500': '#fff2a1',
+  },
+}
